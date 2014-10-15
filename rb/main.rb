28c87f9f9c43029a17d8fc561bade3ea4273d3b3
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-10-15 16:01:12 -0400

    first commit of everything in this project

---
+++ rb/main.rb
@@ -0,0 +1,12 @@
+prompt_for_name = Proc.new do
+    name = $$.prompt("What's your name?")
+    unless name == "" || name == nil
+        $$.alert("Hi, #{name}!")
+    else
+        $$.alert("That's not a valid name.")
+    end
+end
+
+
+logo = $$[:document].getElementById("logo")
+logo.onclick = prompt_for_name
